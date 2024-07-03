21a17421a4e5e8813611e03725774cb2aac96ba0
Piyush Sharma <21367878+piyusharma13@users.noreply.github.com>
2024-07-03 14:35:04 +0530

    Added core functionality

--- Sources/SwiftUICentered/SwiftUICentered.swift
+++ Sources/SwiftUICentered/SwiftUICentered.swift
@@ -1,2 +1,16 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import SwiftUI
+
+extension View{
+    
+    public func alignViewToCenterInHierarchy() -> some View
+    {
+        HStack{
+            Spacer()
+            self
+            Spacer()
+        }
+    }
+    
+}
